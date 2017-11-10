22d48f7018022ae15c55a365d2391a5917046e1b
Katelyn <kaecharbonneau@gmail.com>
2017-11-09 22:02:17 -0500

    A lot of stuff
    Im going to break it soon

--- alarm_clock/alarm_clock.vhd
+++ alarm_clock/alarm_clock.vhd
@@ -14,7 +14,7 @@ ENTITY alarm_clock IS
 		  hours7, hours6: OUT STD_LOGIC_VECTOR(0 TO 6); --SSD for hours
 		  minutes5, minutes4: OUT STD_LOGIC_VECTOR(0 TO 6); --SSD for minutes
 		  seconds3, seconds2: OUT STD_LOGIC_VECTOR(0 TO 6); --SSD for seconds
-		  clock_50MHz: STD_LOGIC); --50MHz Clock
+		  clock_50MHz: IN STD_LOGIC); --50MHz Clock
 END alarm_clock;
 -------------------------------------------------------------------------------
 
@@ -27,13 +27,13 @@ ARCHITECTURE alarm_clock_arch OF alarm_clock IS
 --Debounced Push Buttons
 SIGNAL db_alarm_set, db_clock_set, db_hour_set, db_minute_set: STD_LOGIC;
 
--------------------------------------------------------------------------------
---Component Declarations
+CONSTANT divider: NATURAL := 25000000; --1Hz (1s)
+SIGNAL clock_1Hz: STD_LOGIC;
 
-COMPONENT debounce IS
-	PORT(btn, clk: IN STD_LOGIC;
-			debounced: OUT STD_LOGIC);
-END COMPONENT;
+SIGNAL seconds_ones, seconds_tens,
+			minutes_ones, minutes_tens,
+			hours_ones, hours_tens: NATURAL := 0;
+
 -------------------------------------------------------------------------------
 --BEHAVIOR
 
@@ -45,12 +45,22 @@ clock_btn: debounce PORT MAP (clock_set, clock_50MHz, db_clock_set);
 hour_btn: debounce PORT MAP (hour_set, clock_50MHz, db_hour_set);
 min_btn: debounce PORT MAP (minute_set, clock_50MHz, db_minute_set);
 
+--Produce a clk of Frequency 1Hz (1s)
+slow_the_clock(clock_50MHz, reset, divider, clock_1Hz);
 
---hours7 <= dispSSD(a);
---hours6 <= dispSSD(b);
---minutes5 <= dispSSD(c);
---minutes4 <= dispSSD(d);
---seconds3 <= dispSSD(e);
---seconds2 <= dispSSD(f);
+
+
+--Define clock display behavior
+--THIS INCREASES THE CLOCK OVER TIME
+
+Incrementer: clock_display PORT MAP (clock_1Hz, hours_tens, hours_ones,
+													minutes_tens, minutes_ones, seconds_tens, seconds_ones);
+
+hours7 <= dispSSD(hours_tens);
+hours6 <= dispSSD(hours_ones);
+minutes5 <= dispSSD(minutes_tens);
+minutes4 <= dispSSD(minutes_ones);
+seconds3 <= dispSSD(seconds_tens);
+seconds2 <= dispSSD(seconds_ones);
 
 END alarm_clock_arch;
--- alarm_clock/alarm_clock_package.vhd
+++ alarm_clock/alarm_clock_package.vhd
@@ -3,9 +3,25 @@ USE IEEE.STD_LOGIC_1164.ALL;
 
 PACKAGE alarm_clock_package IS
 -------------------------------------------------------------------------------
+	COMPONENT debounce IS
+		PORT(btn, clk: IN STD_LOGIC;
+				debounced: OUT STD_LOGIC);
+	END COMPONENT;
+-------------------------------------------------------------------------------
+	COMPONENT clock_display IS
+		PORT(clock: IN STD_LOGIC;
+			hours_tens, hours_ones,
+			minutes_tens, minutes_ones,
+			seconds_tens, seconds_ones: IN NATURAL RANGE 0 TO 9);
+	END COMPONENT;
+-------------------------------------------------------------------------------
 	FUNCTION dispSSD (SIGNAL S: NATURAL) RETURN STD_LOGIC_VECTOR;
 -------------------------------------------------------------------------------	
 	FUNCTION rise_edge (SIGNAL CLK: STD_LOGIC) RETURN BOOLEAN;
+-------------------------------------------------------------------------------
+	PROCEDURE slow_the_clock (SIGNAL ip_clock, rst: IN STD_LOGIC;
+										CONSTANT divider: IN NATURAL;
+										SIGNAL op_clock: OUT STD_LOGIC);
 -------------------------------------------------------------------------------
 END alarm_clock_package;
 
@@ -13,7 +29,7 @@ PACKAGE BODY alarm_clock_package IS
 -------------------------------------------------------------------------------
 	FUNCTION dispSSD (SIGNAL S: NATURAL) RETURN STD_LOGIC_VECTOR IS
 		
-	VARIABLE F: STD_LOGIC_VECTOR;
+	VARIABLE F: STD_LOGIC_VECTOR(0 TO 6);
 		
 	BEGIN
 	
@@ -41,5 +57,27 @@ PACKAGE BODY alarm_clock_package IS
 	RETURN (CLK'EVENT AND CLK='1');
 	
 	END FUNCTION rise_edge;
--------------------------------------------------------------------------------	
+-------------------------------------------------------------------------------
+	PROCEDURE slow_the_clock (SIGNAL ip_clock, rst: IN STD_LOGIC;
+										CONSTANT divider: IN NATURAL;
+										SIGNAL op_clock: OUT STD_LOGIC) IS
+										
+	VARIABLE counter: NATURAL;
+	VARIABLE slow_clk: STD_LOGIC := '0';
+	
+	BEGIN
+	
+	IF rise_edge(ip_clock) THEN
+		counter := counter+1;
+		IF (counter = divider) THEN
+			counter := 0;
+			slow_clk := NOT slow_clk;
+		END IF;
+	END IF;
+	IF rst = '1' THEN counter := 0; END IF;
+	
+	op_clock <= slow_clk;
+	
+	END PROCEDURE slow_the_clock;
+-------------------------------------------------------------------------------
 END alarm_clock_package;
