ca554582e39e267bb6da4093dcebaa623281c57d
Katelyn <kaecharbonneau@gmail.com>
2017-11-12 14:54:18 -0500

    cleaned stuff up
    added a ton of comments
    organized code
    got rid of unused dummy variables/signals
    deleted redundant code
    made it a bit more readable in general hopefully

--- alarm_clock/alarm_clock.vhd
+++ alarm_clock/alarm_clock.vhd
@@ -10,10 +10,10 @@ ENTITY alarm_clock IS
 	PORT(alarm_set, clock_set, hour_set, minute_set: IN STD_LOGIC; --Push buttons
 			alarm_status, reset: IN STD_LOGIC; -- Misc inputs
 			alarm: OUT STD_LOGIC; --LEDR0 Alarm Alert
-			am_pm: OUT STD_LOGIC; --LEDG8 AM/PM Indicator (Hour 00-11 = AM/LOW; Hour 12-23 = PM/HIGH);
-			hours7, hours6: OUT STD_LOGIC_VECTOR(0 TO 6); --SSD for hours
-			minutes5, minutes4: OUT STD_LOGIC_VECTOR(0 TO 6); --SSD for minutes
-			seconds3, seconds2: OUT STD_LOGIC_VECTOR(0 TO 6); --SSD for seconds
+			am_pm: OUT STD_LOGIC; --LEDG8 AM/PM Indicator
+			hours7, hours6: OUT STD_LOGIC_VECTOR(0 TO 6); --SSDs for hours
+			minutes5, minutes4: OUT STD_LOGIC_VECTOR(0 TO 6); --SSDs for minutes
+			seconds3, seconds2: OUT STD_LOGIC_VECTOR(0 TO 6); --SSDs for seconds
 			clock_50MHz: IN STD_LOGIC); --50MHz Clock
 END alarm_clock;
 -------------------------------------------------------------------------------
@@ -27,97 +27,77 @@ ARCHITECTURE alarm_clock_arch OF alarm_clock IS
 --Debounced Push Buttons
 SIGNAL db_alarm_set, db_clock_set, db_hour_set, db_minute_set: STD_LOGIC;
 
+--Temp signals for detecting a change in these buttons
+SIGNAL hour_set_change, minute_set_change: STD_LOGIC := '1';
+
+--Information for the 50MHz -> 1Hz slow clock conversion
 CONSTANT divider: NATURAL := 25000000; --1Hz (1s) = 25,000,000
 SIGNAL clock_1Hz: STD_LOGIC := '0';
-SIGNAL meridiem: STD_LOGIC := '0';
-SIGNAL meridiem_alarm: STD_LOGIC := '0';
-
-SIGNAL alarm_enable: STD_LOGIC := '1';
-
-SIGNAL hour_set_change, minute_set_change: STD_LOGIC := '1';
 
 --Values for the clock time
 SIGNAL clock_hours_tens, clock_hours_ones, clock_minutes_tens, clock_minutes_ones,
 			clock_seconds_tens, clock_seconds_ones: NATURAL RANGE 0 TO 9 := 0;
-			
-SIGNAL hours_tens, hours_ones, minutes_tens, minutes_ones,
-			seconds_tens, seconds_ones: NATURAL RANGE 0 TO 9 := 0;
+SIGNAL meridiem: STD_LOGIC := '0';
 
 --Values for the alarm time
 SIGNAL alarm_minutes_ones, alarm_minutes_tens,
 			alarm_hours_ones, alarm_hours_tens,
 			alarm_seconds_ones, alarm_seconds_tens: NATURAL RANGE 0 TO 9 := 0;
-			
-SIGNAL alarm_minutes_ones_process1, alarm_minutes_tens_process1,
-			alarm_hours_ones_process1, alarm_hours_tens_process1,
-			alarm_seconds_ones_process1, alarm_seconds_tens_process1: NATURAL RANGE 0 TO 9 := 0;
-			
-SIGNAL alarm_minutes_ones_process2, alarm_minutes_tens_process2,
-			alarm_hours_ones_process2, alarm_hours_tens_process2,
-			alarm_seconds_ones_process2, alarm_seconds_tens_process2: NATURAL RANGE 0 TO 9 := 0;
+SIGNAL meridiem_alarm: STD_LOGIC := '0';
 
+--Values for the output (select clock or alarm)
+SIGNAL hours_tens, hours_ones, minutes_tens, minutes_ones,
+			seconds_tens, seconds_ones: NATURAL RANGE 0 TO 9 := 0;
 -------------------------------------------------------------------------------
 --BEHAVIOR
 
 BEGIN
-
---Make clock arrays
---clock_time <= (hours_tens, hours_ones, minutes_tens, minutes_ones, seconds_tens, seconds_ones);
---alarm_time <= (alarm_hours_tens, alarm_hours_ones, alarm_minutes_tens, alarm_minutes_ones, 0, 0);
 
 --Debounce the push buttons
 alarm_btn: debounce PORT MAP (alarm_set, clock_50MHz, db_alarm_set);
 clock_btn: debounce PORT MAP (clock_set, clock_50MHz, db_clock_set);
 hour_btn: debounce PORT MAP (hour_set, clock_50MHz, db_hour_set);
 min_btn: debounce PORT MAP (minute_set, clock_50MHz, db_minute_set);
-
+-------------------------------------------------------------------------------
+-------------------------------------------------------------------------------
 --Define clock behavior
-
---SET_CLOCK: PROCESS(clock_50MHz, db_hour_set, db_minute_set)
---BEGIN
---
---END PROCESS;
---	ELSE
-PRODUCE_CLOCK: PROCESS(clock_50MHz, reset, db_clock_set, db_hour_set, db_minute_set)
+PRODUCE_CLOCK: PROCESS(clock_50MHz, reset)
 VARIABLE counter: NATURAL;
---VARIABLE hour_set_change, minute_set_change: STD_LOGIC := '1';
 
 BEGIN
 
 IF rise_edge(clock_50MHz) THEN
 	hour_set_change <= db_hour_set;
 	minute_set_change <= db_minute_set;
+	
+	
 	CASE db_clock_set IS
+	--Implement logic for setting the clock
 	WHEN '0' =>
+		
 		IF (db_hour_set /= hour_set_change AND db_hour_set = '0') THEN
-		alarm_hr_inc(meridiem, clock_hours_tens, clock_hours_ones);
-	END IF;
-	IF (db_minute_set /= minute_set_change AND db_minute_set = '0') THEN
-		alarm_mn_inc(clock_minutes_tens, clock_minutes_ones);
-	END IF;
-	
-	IF reset='1' THEN
-		clock_hours_tens <= 1;
-		clock_hours_ones <= 2;
-		clock_minutes_tens <= 0;
-		clock_minutes_ones <= 0;
-		clock_seconds_tens <= 0;
-		clock_seconds_ones <= 0;
-		meridiem <= '0';
-	END IF;
-	
-	WHEN OTHERS =>
-	counter := counter+1;
-	IF (counter = divider) THEN
-		counter := 0;
-		clock_1Hz <= NOT clock_1Hz;
-		IF clock_1Hz='0' THEN
-			clock_increment(meridiem, clock_hours_tens, clock_hours_ones, clock_minutes_tens, clock_minutes_ones, clock_seconds_tens, clock_seconds_ones);
+			hr_inc(meridiem, clock_hours_tens, clock_hours_ones);
 		END IF;
-	END IF;
+		IF (db_minute_set /= minute_set_change AND db_minute_set = '0') THEN
+			mn_inc(clock_minutes_tens, clock_minutes_ones);
+		END IF;
+-------------------------------------------------------------------------------
+	--Implement logic for incrementing the clock every second
+	WHEN OTHERS =>
+		
+		counter := counter+1;
+		IF (counter = divider) THEN
+			counter := 0;
+			clock_1Hz <= NOT clock_1Hz;
+			IF clock_1Hz='0' THEN
+				clock_increment(meridiem, clock_hours_tens, clock_hours_ones, clock_minutes_tens, clock_minutes_ones, clock_seconds_tens, clock_seconds_ones);
+			END IF;
+		END IF;
+		
 	END CASE;
 END IF;
 
+--Reset logic regardless of state
 IF reset='1' THEN
 	counter := 0;
 	clock_hours_tens <= 1;
@@ -129,54 +109,65 @@ IF reset='1' THEN
 	meridiem <= '0';
 END IF;
 
-END PROCESS;
-						
-SET_ALARM: PROCESS(clock_50MHz, db_hour_set, db_minute_set)
+END PROCESS PRODUCE_CLOCK;
+-------------------------------------------------------------------------------
+-------------------------------------------------------------------------------
+--Define alarm behavior
+SET_ALARM: PROCESS(clock_50MHz, reset, db_alarm_set, db_hour_set, db_minute_set)
 BEGIN
+
+	--Logic for setting the alarm
 	IF (db_alarm_set='0') THEN
 		IF fall_edge(db_hour_set) THEN
-			alarm_hr_inc(meridiem_alarm, alarm_hours_tens, alarm_hours_ones);
+			hr_inc(meridiem_alarm, alarm_hours_tens, alarm_hours_ones);
 		END IF;
 		IF fall_edge(db_minute_set) THEN
-			alarm_mn_inc(alarm_minutes_tens, alarm_minutes_ones);
+			mn_inc(alarm_minutes_tens, alarm_minutes_ones);
 		END IF;
 	END IF;
 	
+	--Reset logic
 	IF reset = '1' THEN
 		alarm_hours_tens <= 1;
 		alarm_hours_ones <= 2;
 		alarm_minutes_tens<= 0;
 		alarm_minutes_ones <= 0;
-		alarm_seconds_tens <= 0;
-		alarm_seconds_ones <= 0;
 		meridiem_alarm <= '0';
 	END IF;
-END PROCESS;
+END PROCESS SET_ALARM;
 
+--These are always 0
+alarm_seconds_tens <= 0;
+alarm_seconds_ones <= 0;
+-------------------------------------------------------------------------------
+-------------------------------------------------------------------------------
+--Define alarm LED Output
 alarm <= test_alarm(alarm_status, meridiem, meridiem_alarm,
 							clock_hours_tens, alarm_hours_tens,
 							clock_hours_ones, alarm_hours_ones,
 							clock_minutes_tens, alarm_minutes_tens,
 							clock_minutes_ones, alarm_minutes_ones);
-							
+-------------------------------------------------------------------------------
+--Select each output based on whether the clock or alarm time should be displayed
 hours_tens <= alarm_hours_tens WHEN db_alarm_set = '0' ELSE
 					clock_hours_tens;
-					
+
 hours_ones <= alarm_hours_ones WHEN db_alarm_set = '0' ELSE
 					clock_hours_ones;
-					
+
 minutes_tens <= alarm_minutes_tens WHEN db_alarm_set = '0' ELSE
 						clock_minutes_tens;
-						
+
 minutes_ones <= alarm_minutes_ones WHEN db_alarm_set = '0' ELSE
 						clock_minutes_ones;
-						
+
 seconds_tens <= alarm_seconds_tens WHEN db_alarm_set = '0' ELSE
 						clock_seconds_tens;
-						
+
 seconds_ones <= alarm_seconds_ones WHEN db_alarm_set = '0' ELSE
 						clock_seconds_ones;
-
+-------------------------------------------------------------------------------
+--Apply the selection from the above section to the output ports
 hours7 <= dispSSD(hours_tens);
 hours6 <= dispSSD(hours_ones);
 minutes5 <= dispSSD(minutes_tens);
--- alarm_clock/alarm_clock_package.vhd
+++ alarm_clock/alarm_clock_package.vhd
@@ -1,47 +1,53 @@
 LIBRARY IEEE;
 USE IEEE.STD_LOGIC_1164.ALL;
+-------------------------------------------------------------------------------
 
+-------------------------------------------------------------------------------
 PACKAGE alarm_clock_package IS
 -------------------------------------------------------------------------------
-	TYPE my_time IS ARRAY (7 DOWNTO 2) OF NATURAL;
--------------------------------------------------------------------------------
+--Used for debouncing switches
 	COMPONENT debounce IS
 		PORT(btn, clk: IN STD_LOGIC;
 				debounced: OUT STD_LOGIC);
 	END COMPONENT;
 -------------------------------------------------------------------------------
+--Seven-Segment Display logic
 	FUNCTION dispSSD (SIGNAL S: NATURAL) RETURN STD_LOGIC_VECTOR;
--------------------------------------------------------------------------------	
+-------------------------------------------------------------------------------
+--Custom simple rising edge Function
 	FUNCTION rise_edge (SIGNAL CLK: STD_LOGIC) RETURN BOOLEAN;
 -------------------------------------------------------------------------------
+--Custom simple falling edge function
 	FUNCTION fall_edge (SIGNAL CLK: STD_LOGIC) RETURN BOOLEAN;
 -------------------------------------------------------------------------------
+--Logic to set the alarm output
 	FUNCTION test_alarm (SIGNAL enable, mer1, mer2: STD_LOGIC;
 								SIGNAL h1, h2, h3, h4,
 								m1, m2, m3, m4: NATURAL RANGE 0 TO 9) RETURN STD_LOGIC;
 -------------------------------------------------------------------------------
-	PROCEDURE slow_the_clock (SIGNAL ip_clock, rst: IN STD_LOGIC;
-										CONSTANT divider: IN NATURAL;
-										SIGNAL op_clock: OUT STD_LOGIC);
--------------------------------------------------------------------------------
+--Logic to increment clock every time this procedure is ran
 	PROCEDURE clock_increment (SIGNAL meridiem: INOUT STD_LOGIC;
 										SIGNAL hours_tens, hours_ones,
 										minutes_tens, minutes_ones,
 										seconds_tens, seconds_ones: INOUT NATURAL RANGE 0 TO 9);
 -------------------------------------------------------------------------------
-	PROCEDURE alarm_hr_inc (SIGNAL meridiem_alarm: INOUT STD_LOGIC;
-									SIGNAL alarm_hours_tens, alarm_hours_ones: INOUT NATURAL RANGE 0 TO 9);
+--Logic to increment hours
+	PROCEDURE hr_inc (SIGNAL meridiem_alarm: INOUT STD_LOGIC;
+							SIGNAL alarm_hours_tens, alarm_hours_ones: INOUT NATURAL RANGE 0 TO 9);
 -------------------------------------------------------------------------------
-	PROCEDURE alarm_mn_inc (SIGNAL alarm_minutes_tens, alarm_minutes_ones: INOUT NATURAL RANGE 0 TO 9);
+--Logic to increment minutes
+	PROCEDURE mn_inc (SIGNAL alarm_minutes_tens, alarm_minutes_ones: INOUT NATURAL RANGE 0 TO 9);
 -------------------------------------------------------------------------------
 END alarm_clock_package;
+-------------------------------------------------------------------------------
 
+-------------------------------------------------------------------------------
 PACKAGE BODY alarm_clock_package IS
 -------------------------------------------------------------------------------
 	FUNCTION dispSSD (SIGNAL S: NATURAL) RETURN STD_LOGIC_VECTOR IS
-		
+	
 	VARIABLE F: STD_LOGIC_VECTOR(0 TO 6);
-		
+	
 	BEGIN
 	
 	CASE S IS
@@ -57,8 +63,8 @@ PACKAGE BODY alarm_clock_package IS
 		WHEN 9 => F := "0000100";
 		WHEN OTHERS => F := "1111110";
 	END CASE;
-			
-		RETURN F;
+	
+	RETURN F;
 	END FUNCTION dispSSD;
 -------------------------------------------------------------------------------
 	FUNCTION rise_edge (SIGNAL CLK: STD_LOGIC) RETURN BOOLEAN IS
@@ -81,35 +87,13 @@ PACKAGE BODY alarm_clock_package IS
 								SIGNAL h1, h2, h3, h4,
 								m1, m2, m3, m4: NATURAL RANGE 0 TO 9) RETURN STD_LOGIC IS
 	BEGIN
-	
+		
 		IF (h1 = h2 AND h3 = h4 AND m1 = m2 AND m3 = m4 AND mer1 = mer2 AND enable = '0') THEN
 			RETURN '1';
 		ELSE
 			RETURN '0';
 		END IF;
 	END FUNCTION test_alarm;
--------------------------------------------------------------------------------
-	PROCEDURE slow_the_clock (SIGNAL ip_clock, rst: IN STD_LOGIC;
-										CONSTANT divider: IN NATURAL;
-										SIGNAL op_clock: OUT STD_LOGIC) IS
-										
-	VARIABLE counter: NATURAL;
-	VARIABLE slow_clk: STD_LOGIC := '0';
-	
-	BEGIN
-	
-	IF rise_edge(ip_clock) THEN
-		counter := counter+1;
-		IF (counter = divider) THEN
-			counter := 0;
-			slow_clk := NOT slow_clk;
-		END IF;
-	END IF;
-	IF rst = '1' THEN counter := 0; END IF;
-	
-	op_clock <= slow_clk;
-	
-	END PROCEDURE slow_the_clock;
 -------------------------------------------------------------------------------
 	PROCEDURE clock_increment (SIGNAL meridiem: INOUT STD_LOGIC;
 										SIGNAL hours_tens, hours_ones,
@@ -146,10 +130,10 @@ PACKAGE BODY alarm_clock_package IS
 		END IF;
 	END IF;
 	
-	END clock_increment;
+	END PROCEDURE clock_increment;
 -------------------------------------------------------------------------------
-	PROCEDURE alarm_hr_inc (SIGNAL meridiem_alarm: INOUT STD_LOGIC;
-									SIGNAL alarm_hours_tens, alarm_hours_ones: INOUT NATURAL RANGE 0 TO 9) IS
+	PROCEDURE hr_inc (SIGNAL meridiem_alarm: INOUT STD_LOGIC;
+							SIGNAL alarm_hours_tens, alarm_hours_ones: INOUT NATURAL RANGE 0 TO 9) IS
 
 	BEGIN
 		alarm_hours_ones <= alarm_hours_ones+1;
@@ -164,9 +148,9 @@ PACKAGE BODY alarm_clock_package IS
 		IF(alarm_hours_tens >= 1 AND alarm_hours_ones = 1) THEN
 			meridiem_alarm <= NOT meridiem_alarm;
 		END IF;
-	END alarm_hr_inc;
+	END PROCEDURE hr_inc;
 -------------------------------------------------------------------------------
-	PROCEDURE alarm_mn_inc (SIGNAL alarm_minutes_tens, alarm_minutes_ones: INOUT NATURAL RANGE 0 TO 9) IS
+	PROCEDURE mn_inc (SIGNAL alarm_minutes_tens, alarm_minutes_ones: INOUT NATURAL RANGE 0 TO 9) IS
 
 	BEGIN
 		alarm_minutes_ones <= alarm_minutes_ones+1;
@@ -177,6 +161,6 @@ PACKAGE BODY alarm_clock_package IS
 				alarm_minutes_tens <= 0;
 			END IF;
 		END IF;
-	END alarm_mn_inc;
+	END PROCEDURE mn_inc;
 -------------------------------------------------------------------------------
 END alarm_clock_package;
